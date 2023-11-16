a15b91ca6ffec749ff329b9409bae1d22174cdb2
Muhammad3921 <apnahasnain@gmail.com>
2023-11-16 16:05:07 -0500

    Added facial detection functionality

--- Main.py
+++ Main.py
@@ -1,13 +1,37 @@
 #Main python file to initiate video and microphone
 import cv2
+import face_recognition
+import os
+import time
 
+fn = 'screenshot.jpg'
+prev_time = os.path.getmtime(fn)
+t = prev_time
+
+t1 = face_recognition.load_image_file("training/nyle_train1.jpg")
+t2 = face_recognition.load_image_file("training/nyle_train2.jpg")
+t3 = face_recognition.load_image_file("training/nyle_train3.png")
+t4 = face_recognition.load_image_file("training/nyle_train4.jpg")
+t5 = face_recognition.load_image_file("training/nyle_train5.jpg")
+
+nyle1 = face_recognition.face_encodings(t1)[0]
+nyle2 = face_recognition.face_encodings(t2)[0]
+nyle3 = face_recognition.face_encodings(t3)[0]
+nyle4 = face_recognition.face_encodings(t4)[0]
+nyle5 = face_recognition.face_encodings(t5)[0]
+
+nyle = [
+    nyle1, nyle2, nyle3, nyle4, nyle5
+]
 # Load the pre-trained Haarcascades face detection model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Open the camera (0 represents the default camera)
 cap = cv2.VideoCapture(0)
 
-while True:
+counter = 0
+while counter !=40:
+    print(counter)
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -22,15 +46,21 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         # Save a screenshot when a face is detected
+        counter= counter+1
         cv2.imwrite('screenshot.jpg', frame)
-        #fork here to do facial recog and see what person it is break aswell
 
     # Display the resulting frame
     cv2.imshow('Face Detection', frame)
-
     # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+time.sleep(5)
+t6 = face_recognition.load_image_file("screenshot.jpg")
+CurrentFace = face_recognition.face_encodings(t6)[0]
+r1 = face_recognition.compare_faces(nyle, CurrentFace)
+print(r1)
+
 
 # Release the camera and close all OpenCV windows
 cap.release()
